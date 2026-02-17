6e11857aac7253df00235ee8a63a8b2230f65508
kmj <sokkomann@gmail.com>
2026-02-18 00:25:26 +0900

    일반,판매자 회원가입

--- src/main/resources/sql/test.sql
+++ src/main/resources/sql/test.sql
@@ -25,6 +25,8 @@ select * from tbl_user;
 
 select * from tbl_auth;
 
+select * from tbl_seller;
+
 # 확인용 유저 샘플
 insert into tbl_user (id, user_email, user_phone, user_name, user_intro)
 values (1, 'example@example.com', '01012345678', '홍길동', '설명1');
