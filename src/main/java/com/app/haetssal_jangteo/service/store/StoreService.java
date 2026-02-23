20e3ef916e8a30be4f31b040f92758edc14ad064
ljs528022 <ljs528022@gmail.com>
2026-02-23 09:07:17 +0900

    가게 수정 기능 중간 save  #37

--- src/main/java/com/app/haetssal_jangteo/service/store/StoreService.java
+++ src/main/java/com/app/haetssal_jangteo/service/store/StoreService.java
@@ -53,7 +53,7 @@ public class StoreService {
         UUID uuid = UUID.randomUUID();
         fileDTO.setFileSavedPath(todayPath);
         fileDTO.setFileSize(String.valueOf(multipartFile.getSize()));
-        fileDTO.setFileOriginName(multipartFile.getOriginalFilename());
+        fileDTO.setFileOriginalName(multipartFile.getOriginalFilename());
         fileDTO.setFileName(uuid.toString() + "_" + multipartFile.getOriginalFilename());
         fileDTO.setFileType(multipartFile.getContentType().contains("image") ? Filetype.IMAGE : Filetype.DOCUMENT);
         fileDAO.save(fileDTO);
@@ -92,7 +92,7 @@ public class StoreService {
         UUID uuid = UUID.randomUUID();
         fileDTO.setFileSavedPath(todayPath);
         fileDTO.setFileSize(String.valueOf(multipartFile.getSize()));
-        fileDTO.setFileOriginName(multipartFile.getOriginalFilename());
+        fileDTO.setFileOriginalName(multipartFile.getOriginalFilename());
         fileDTO.setFileName(uuid.toString() + "_" + multipartFile.getOriginalFilename());
         fileDTO.setFileType(multipartFile.getContentType().contains("image") ? Filetype.IMAGE : Filetype.DOCUMENT);
         fileDAO.save(fileDTO);
